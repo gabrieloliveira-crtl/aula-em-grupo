e259c78408af9e4326b2409eb2e81349ed5849d3
Ana28373 <162150289+Ana28373@users.noreply.github.com>
2024-08-05 12:27:11 -0300

    mudei a tabela de divisao

---
+++ divisao.py
@@ -0,0 +1,6 @@
+numero = int(input("insira um número: "))
+
+print("tabela de multiplicação para", numero,":")
+for i in range(1, 11):
+    produto = numero / i
+    print(numero, "x", i, "=", produto)
